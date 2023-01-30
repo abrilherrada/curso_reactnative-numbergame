fa874bd6c430143058ece5d4250f43951b8a16ee
Abril Herrada <abrilherrada@gmail.com>
2023-01-30 13:03:11 -0300

    style constants added and implemented

---
+++ src/constants/themes/colors.js
@@ -0,0 +1,10 @@
+export const colors = {
+  primary: "#5E548E",
+  secondary: "#7B72AC",
+  primaryButton: "#B6C8A9",
+  secondaryButton: "#F4A4A6",
+  background: "#EBF2FA",
+  text: "#212121",
+  white: "#fff",
+  black: "#000",
+};
--- src/screens/start-game/index.jsx
+++ src/screens/start-game/index.jsx
@@ -3,6 +3,7 @@ import { View, Text, TextInput, Button } from "react-native";
 
 import { styles } from "./styles";
 import Card from "../../components/card";
+import { colors } from "../../constants";
 
 export const StartGame = () => {
   return (
@@ -12,8 +13,8 @@ export const StartGame = () => {
         <Text style={styles.label}>Escribe un número</Text>
         <TextInput keyboardType="numeric" style={styles.input} placeholder="0" />
         <View style={styles.buttonContainer}>
-          <Button title="Reiniciar" onPress={() => null} color="#7B72AC" />
-          <Button title="Confirmar" onPress={() => null} color="#5E548E" />
+          <Button title="Reiniciar" onPress={() => null} color={colors.secondaryButton} />
+          <Button title="Confirmar" onPress={() => null} color={colors.primaryButton} />
         </View>
       </Card>
     </View>
