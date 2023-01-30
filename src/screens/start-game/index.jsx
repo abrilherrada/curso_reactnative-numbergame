e63102b883868a16d27999a19ab2506ac14dfdf3
Abril Herrada <abrilherrada@gmail.com>
2023-01-30 01:06:15 -0300

    layout in start game screen and styles created

--- src/screens/start-game/index.jsx
+++ src/screens/start-game/index.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TextInput, Button } from "react-native";
 
 import { styles } from "./styles";
 
 export const StartGame = () => {
   return (
     <View style={styles.container}>
-      <Text>Empezar</Text>
+      <Text style={styles.title}>Empezar</Text>
+      <View style={styles.inputContainer}>
+        <Text style={styles.label}>Escribe un número.</Text>
+        <TextInput style={styles.input} placeholder="0" />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title="Reiniciar" onPress={() => null} color="#7B72AC" />
+        <Button title="Confirmar" onPress={() => null} color="#5E548E" />
+      </View>
     </View>
   );
 };
