2a75e2a4eaf193a11db40ec393afbe39da0d489a
Abril Herrada <abrilherrada@gmail.com>
2023-01-30 00:41:02 -0300

    start game screen created

---
+++ src/screens/start-game/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import { styles } from "./styles";
+
+export const StartGame = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Empezar</Text>
+    </View>
+  );
+};
+
+export default StartGame;
